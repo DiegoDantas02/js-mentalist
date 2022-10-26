f40eabf816ae99a66fd9b58752a65bc7894bae55
Diego R Dantas <112526586+DiegoDantas02@users.noreply.github.com>
2022-10-25 21:07:51 -0300

    js-mentalist

---
+++ app.js
@@ -0,0 +1,17 @@
+function Chutar() {
+    let numeroSecreto = Math.floor(Math.random() * 10)
+    let chute = document.getElementById("valor").value
+
+    let emojiFeliz = '<img src="imagens/emojiFeliz.png" width="50" height="50">'
+    let emojiTriste = '<img src="imagens/emojiTriste.png" width="50" height="50">'
+
+    if (chute == numeroSecreto) {
+        //vdd
+        document.getElementById("resultado").innerHTML = emojiFeliz;
+    }else {
+    //falso
+    document.getElementById("resultado").innerHTML = emojiTriste;
+ }
+}
+
+
